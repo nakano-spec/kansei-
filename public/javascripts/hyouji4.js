b74283f0962c0cbd317db11b69012bf98841f1cb
nakano-spec <93574762+nakano-spec@users.noreply.github.com>
2023-01-26 15:14:21 +0900

    commit

--- public/javascripts/hyouji4.js
+++ public/javascripts/hyouji4.js
@@ -1,36 +1,43 @@
-//サーバーから送られてきた配列データをJSON形式で格納し、中のデータの数分表を作り、結果に合わせて表の色を変えている。
 var socket = io();
 const rows = JSON.parse('<%- JSON.stringify(han1) %>');
 var rowlen = rows.length;
+console.log(rowlen);
 var rowlen2 = parseInt(rowlen/12) + 1;
+console.log(rowlen2);
 var a = 0;
 var b = 0;
 var c = 0;
 var myd = document.getElementById("box");
 var i = 0;
 var j = 0;
+var k = 0;
 var table1 = document.createElement("table");
 var tbody1 = document.createElement("tbody");
+var tr1 = document.createElement("tr");
 while(i < rowlen2){
-    var tr1 = document.createElement("tr");
-    while(j < rowlen && j != 12){
+    tr1 = document.createElement("tr");
+    while(k < rowlen && j != 12){
       var td1 = document.createElement("td");  
-      a = rows[j].username;
+      a = rows[k].username;
       td1.textContent = a;
-      b = rows[j].han;
+      b = rows[k].han;
       tr1.appendChild(td1);
       if(b === "○"){
         td1.setAttribute("bgcolor","#FF0000");
       }else if(b === "✕"){
         td1.setAttribute("bgcolor","#0000FF");
       }
-      td1.setAttribute("width","80px");
-      td1.setAttribute("height","80px");
+      td1.setAttribute("width","160px");
+      td1.setAttribute("height","160px");
       td1.setAttribute("align","center");
-      j = j+1;
+      j = j+1; 
+      k = k + 1;
     }
+    console.log(tr1);
     tbody1.appendChild(tr1);
     i = i + 1;
+    j = 0;
+   
 }
 table1.appendChild(tbody1);
 myd.appendChild(table1);
--- routes/kakunin.js
+++ routes/kakunin.js
@@ -15,13 +15,17 @@ router.get('/', function(req, res, next) {
    var app = req.app;
    var poolCluster = app.get("pool");
    var pool = poolCluster.of('MASTER')
-   var sql4 = "select m.name,m.mondaibun,m.sen1,m.sen2,m.sen3,m.sen4,s.seikai from mondai_LIST m,seikai_LIST s where name = ? and m.seikai_ID = s.seikai_ID and m.mon_ID = s.mon_ID;"
+   var sql4 = "select m.name,m.mondaibun,m.sen1,m.sen2,m.sen3,m.sen4,s.seikai,m.picturename,m.kai_ID from mondai_LIST m,seikai_LIST s where name = ? and m.seikai_ID = s.seikai_ID and m.mon_ID = s.mon_ID;"
    pool.getConnection(function(err,connection){
     connection.query(sql4,data1,(err,result,fields) =>{
         if(err){
             console.log(err);
         }
-        res.render('kakunin.ejs',{data:result});
+        if(result[0].kai_ID == 1){
+            res.render('kakunin2.ejs',{data:result});
+        }else{
+            res.render('kakunin.ejs',{data:result});
+        }
         connection.release();
    })
 
